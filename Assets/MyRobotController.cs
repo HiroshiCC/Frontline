b8bbf5cb9a9261a5f2b7b15717b3b7af088bacd7
高須　寛 <h-takasu@mwa.biglobe.ne.jp>
2017-09-11 15:03:29 +0900

    オリジナルゲーム試作
    自分自身の移動を研究中

---
+++ Assets/MyRobotController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyRobotController : MonoBehaviour {
+
+    private GameObject myCamera;
+    private Rigidbody myRigidbody;
+
+    // Use this for initialization
+    void Start () {
+        myCamera = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 左に向く
+        if (Input.GetKey(KeyCode.LeftArrow))
+             transform.Rotate(0, -2, 0);
+ 
+        // 右に向く
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(0, 2, 0);
+
+		// 前進
+		if (Input.GetKey(KeyCode.A))
+			myRigidbody.AddForce(transform.forward * 100f );
+
+
+		// 後退
+		if (Input.GetKey(KeyCode.Z))
+            myRigidbody.AddForce(-transform.forward * 100f );
+
+
+		// カメラの位置は、MyCameraController.csで処理
+	}
+}
